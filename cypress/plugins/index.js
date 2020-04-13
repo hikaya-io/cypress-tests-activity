d489331f5223f649cdf1125e6629e7adf90a8de1
anastiour@gmail.com <anas@cedrus.ma>
2020-04-13 13:53:27 +0100

    VT: configure cypress-plugin-snapshots

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,7 +8,7 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -16,6 +16,8 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  initPlugin(on, config);
+  return config;
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
