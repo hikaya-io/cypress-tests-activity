3f5be01f3ae090014969494140f97dfe2a814a7b
Amos Njoroge <sannleen@users.noreply.github.com>
2020-04-14 12:31:11 +0300

    Deleted users fixtures

---
+++ cypress/integration/sanity/ATCATC-2_Add_Edit_Delete_Program.spec.js
@@ -0,0 +1,80 @@
+/// <reference types="Cypress" />
+
+context("Registration", () => {
+    let data;
+    let env;
+
+    before(() => {
+        cy.fixture("test_data.json").then(test_data => {
+            data = test_data;
+        });
+        env = Cypress.env();
+    });
+    it("ACTATC-2.1 Adds a New Program", () => {
+        const today = new Date();
+        const today1 = new Date();
+        //const endDate = today.setMonth(today.getDate() - 1);
+
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        const yyyy = today.getFullYear();
+        today1.setMonth(today1.getMonth() + 1);
+        let ddE = today1.getDate();
+        let mmE = today1.getMonth() + 1;
+        const yyyyE = today1.getFullYear();
+
+        if (mmE < 10) {
+            mmE = `0${mmE}`;
+        }
+        const startDate = `${`0${dd}`.substring(
+            `0${dd}`.length - 2,
+            `0${dd}`.length
+        )}.${`0${mm}`.substring(
+            `0${mm}`.length - 2,
+            `0${mm}`.length
+        )}.${yyyyE}`;
+        const endDate = `${`0${ddE}`.substring(
+            `0${ddE}`.length - 2,
+            `0${ddE}`.length
+        )}.${`0${mmE}`.substring(
+            `0${mmE}`.length - 2,
+            `0${mmE}`.length
+        )}.${yyyyE}`;
+
+        cy.loginByCSRF(env.un_qa_org, env.pw_qa_org);
+
+        cy.visit("/");
+        cy.get(".dropdown")
+            .contains("Workflow")
+            .click()
+            .then(() => {
+                cy.get("a")
+                    .contains("Programs")
+                    .click();
+                cy.get(".page-title").should("contain", "Programs List");
+            });
+
+        cy.get("[data-target='#addProgramModal'").click();
+        cy.get("#addProgramModal").should("be.visible");
+        cy.get("#addProgramModalLabel").should("contain", "Add Programs");
+
+        cy.get("#programName")
+            .invoke("val", data.new_program)
+            .should("have.value", data.new_program);
+        cy.get("[placeholder='Select sectors...']").click();
+        cy.get(".select2-results__option")
+            .contains(data.sector_DRR)
+            .click("center", { force: true });
+        cy.get(`li[title="${data.sector_DRR}"]`).should("exist");
+
+        cy.get("#startDate").type(startDate);
+        cy.get("#endDate").type(endDate);
+
+        cy.get("a[title='Prev']").click();
+        // cy.get("a[title='Prev']").should("have.class", "ui-state-disabled");
+
+        cy.get("#startDate").click();
+        cy.get("a[title='Next']").click();
+        cy.get("a[title='Next']").should("have.class", "ui-state-disabled");
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,7 @@ const { MailSlurp } = require("mailslurp-client");
 const JSSoup = require("jssoup").default;
 
 const mailslurp = new MailSlurp({
-    apiKey: Cypress.env("api_key_mailslurp")
+    apiKey: Cypress.env("api_key_mailslurp"),
 });
 
 // Generate a random email
@@ -11,7 +11,7 @@ Cypress.Commands.add("newEmailAddress", () => {
 });
 
 // Fetch email by it index (0 based index)
-Cypress.Commands.add("getEmail", count => {
+Cypress.Commands.add("getEmail", (count) => {
     const email = mailslurp.waitForNthEmail(
         Cypress.env("mailslurp_inbox"),
         count,
@@ -22,13 +22,13 @@ Cypress.Commands.add("getEmail", count => {
 
 // Extract notifcation link from email
 Cypress.Commands.add("getLinkFromEmail", (subject, link_title, count) => {
-    cy.getEmail(count).then(email => {
+    cy.getEmail(count).then((email) => {
         expect(email.subject).to.equal(subject);
         const soup = new JSSoup(email.body);
         const links = soup.findAll("a");
         let link_found;
 
-        links.forEach(link => {
+        links.forEach((link) => {
             if (link.attrs.title == link_title) {
                 link_found = link.attrs.href;
             }
@@ -61,18 +61,16 @@ Cypress.Commands.add("login", (email, password) => {
 
 Cypress.Commands.add("enterLoginDetails", (email, password) => {
     cy.visit("/accounts/login");
-    cy.get("#login_username")
-        .type(email)
-        .should("have.value", email);
-    cy.get("#login_password")
-        .type(password)
-        .should("have.value", password);
+    cy.get("#login_username").type(email).should("have.value", email);
+    cy.get("#login_password").type(password).should("have.value", password);
     cy.get("#login_submit_btn").click();
 });
 
+///////////////////////////////////////// Custom API commands /////////////////////////////////////////
+
 // Bypass UI login and login using CSRF Tokens
 Cypress.Commands.add("loginByCSRF", (username, password) => {
-    cy.request("/accounts/login/").then(res => {
+    cy.request("/accounts/login/").then((res) => {
         const $html = Cypress.$(res.body);
         const csrfToken = $html.find("input[name=csrfmiddlewaretoken]").val();
         cy.request({
@@ -83,40 +81,45 @@ Cypress.Commands.add("loginByCSRF", (username, password) => {
             body: {
                 username,
                 password,
-                csrfmiddlewaretoken: csrfToken // insert this as part of form body
+                csrfmiddlewaretoken: csrfToken, // insert this as part of form body
             },
             headers: {
-                Referer: Cypress.config().baseUrl
-            }
-        }).then(res => {
+                Referer: Cypress.config().baseUrl,
+            },
+        }).then((res) => {
             expect(res.status).to.eq(200);
         });
     });
 });
 
 // Login into django Admin using CSRF Tokens
-Cypress.Commands.add("loginAdmin", (username, password) => {
-    cy.request("/admin/login/?next=/admin/").then(res => {
-        const $html = Cypress.$(res.body);
-        const csrfToken = $html.find("input[name=csrfmiddlewaretoken]").val();
-        cy.request({
-            method: "POST",
-            url: "admin/login/?next=/admin/",
-            failOnStatusCode: false,
-            form: true,
-            body: {
-                username,
-                password,
-                csrfmiddlewaretoken: csrfToken
-            },
-            headers: {
-                Referer: Cypress.config().baseUrl + "/admin/login/?next=/admin/"
-            }
-        }).then(res => {
-            expect(res.status).to.eq(200);
+Cypress.Commands.add(
+    "loginAdmin",
+    (
+        username = Cypress.env("un_qa_org"),
+        password = Cypress.env("pw_qa_org")
+    ) => {
+        cy.request("/admin/login/?next=/admin/").then((res) => {
+            const $html = Cypress.$(res.body);
+            const csrfToken = $html
+                .find("input[name=csrfmiddlewaretoken]")
+                .val();
+            cy.request({
+                method: "POST",
+                url: "admin/login/?next=/admin/",
+                failOnStatusCode: false,
+                form: true,
+                body: {
+                    username,
+                    password,
+                    csrfmiddlewaretoken: csrfToken,
+                },
+            }).then((res) => {
+                expect(res.status).to.eq(200);
+            });
         });
-    });
-});
+    }
+);
 
 // Logout into django Admin
 Cypress.Commands.add("logoutAdmin", () => {
@@ -125,7 +128,7 @@ Cypress.Commands.add("logoutAdmin", () => {
 
 // Add a user using django admin
 Cypress.Commands.add("addUser", (username, password, email) => {
-    cy.request("/admin/auth/user/").then(res => {
+    cy.request("/admin/auth/user/").then((res) => {
         const link = Cypress.$(res.body)
             .find(`a:contains('${username}')`)
             .attr("href");
@@ -145,12 +148,12 @@ Cypress.Commands.add("addUser", (username, password, email) => {
                     username: username,
                     email: email,
                     password1: password,
-                    password2: password
+                    password2: password,
                 },
                 headers: {
-                    Referer: Cypress.config().baseUrl
-                }
-            }).then(res => {
+                    Referer: Cypress.config().baseUrl,
+                },
+            }).then((res) => {
                 expect(res.status).to.eq(200);
                 const $html = Cypress.$(res.body);
                 const success = $html.find("li[class='success']").text();
@@ -175,12 +178,12 @@ Cypress.Commands.add("addUser", (username, password, email) => {
                     body: {
                         csrfmiddlewaretoken: csrfToken,
                         user: userId,
-                        _save: "Save"
+                        _save: "Save",
                     },
                     headers: {
-                        Referer: Cypress.config().baseUrl
-                    }
-                }).then(res => {
+                        Referer: Cypress.config().baseUrl,
+                    },
+                }).then((res) => {
                     expect(res.status).to.eq(200);
                     const $html = Cypress.$(res.body);
                     const success = $html.find("li[class='success']").text();
@@ -197,7 +200,7 @@ Cypress.Commands.add("addUser", (username, password, email) => {
 
 // Update a user using django admin TODO: Unfinished
 Cypress.Commands.add("updateUser", (fName, lName) => {
-    cy.request("/admin/auth/user/").then(res => {
+    cy.request("/admin/auth/user/").then((res) => {
         const $html = Cypress.$(res.body);
         const link = $html.find("a:contains('gfxffg')").attr("href");
         const csrfToken = res.headers["set-cookie"][0]
@@ -214,12 +217,12 @@ Cypress.Commands.add("updateUser", (fName, lName) => {
                 csrfmiddlewaretoken: csrfToken,
                 first_name: "testXXX",
                 last_name: "GGXXX",
-                _save: "Save"
+                _save: "Save",
             },
             headers: {
-                Referer: Cypress.config().baseUrl
-            }
-        }).then(res => {
+                Referer: Cypress.config().baseUrl,
+            },
+        }).then((res) => {
             expect(res.status).to.eq(200);
             cy.visit(link);
         });
@@ -227,8 +230,8 @@ Cypress.Commands.add("updateUser", (fName, lName) => {
 });
 
 // Delete a user using django admin [TODO]: Ensure exact user is deleted
-Cypress.Commands.add("deleteUser", username => {
-    cy.request("/admin/auth/user/").then(res => {
+Cypress.Commands.add("deleteUser", (username) => {
+    cy.request("/admin/auth/user/").then((res) => {
         const link = Cypress.$(res.body)
             .find(`a:contains('${username}')`)
             .attr("href");
@@ -249,12 +252,12 @@ Cypress.Commands.add("deleteUser", username => {
                     csrfmiddlewaretoken: csrfToken,
                     _selected_action: id,
                     action: "delete_selected",
-                    post: "yes"
+                    post: "yes",
                 },
                 headers: {
-                    Referer: Cypress.config().baseUrl
-                }
-            }).then(res => {
+                    Referer: Cypress.config().baseUrl,
+                },
+            }).then((res) => {
                 expect(res.status).to.eq(200);
                 const $html = Cypress.$(res.body);
                 const success = $html.find("li[class='success']").text();
